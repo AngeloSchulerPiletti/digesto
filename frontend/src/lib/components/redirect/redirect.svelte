b43b2e15f5a01542ed7b34cccd0f7646b55dcada
Lucas Fernandes <me@lucasfernandes.com.br>
2022-06-05 17:35:14 -0300

    Add redirect component

---
+++ frontend/src/lib/components/redirect/redirect.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+
+  export let url: string;
+
+  onMount(() => goto(url));
+</script>
+
+<div class="redirect">
+  <img class="logo" src="/digesto-icon.png" alt="" />
+</div>
+
+<style lang="sass">
+  .redirect
+    display: flex
+    justify-content: center
+    align-items: center
+    width: 100%
+    height: 100%
+
+    .logo
+      height: 6rem
+      pointer-events: none
+      user-select: none
+      animation: blink 2s ease infinite alternate
+      
+  @keyframes blink
+    from
+      opacity: .25
+
+    to
+      opacity: 1
+</style>
