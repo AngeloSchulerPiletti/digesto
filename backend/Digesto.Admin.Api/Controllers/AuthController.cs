10b22007199e83444ed824794bf3d7011840e040
SchPil <angelopiletti@gmail.com>
2022-06-05 16:28:16 -0300

    Admin api created

---
+++ backend/Digesto.Admin.Api/Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Digesto.Admin.Api.Controllers;
+
+[Route("api/admin/auth")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+}
